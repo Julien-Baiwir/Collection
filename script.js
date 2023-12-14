e28e21745f0af662bd795107dc1ff2452cfd7dd2
Julien Baiwir <julien.freddy.baiwir@gmail.com>
2023-12-14 12:22:39 +0100

    version2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Collection zombies films--------------------
 let films = [
     {
-        Image: "../images/1zombies.jpg",
+        Image: "images/1zombies.jpg",
         Titre: "Zombies",
         Resume: "Quand le monde est en ruine, ravagé par une épidémie de zombies, Luke et son équipe auront-ils assez de force et de munitions?",
         Acteurs: ["Tony Todd", "Steven Luke", "Raina Hein"],
@@ -9,7 +9,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt4969672/"
     },
     {
-        Image: "../images/2corona-zombies.jpg",
+        Image: "images/2corona-zombies.jpg",
         Titre: "Corona Zombies",
         Resume: "Des demoiselles étourdies en détresse, une famine de papier toilette, des médias de masse déchaînés et un virus infernal!",
         Acteurs: ["Cody Renee", "Russell Coker", "Robin Sydney"],
@@ -18,7 +18,7 @@ let films = [
     },
 
     {
-        Image: "../images/3bigfoot-vs-zombies.jpg",
+        Image: "images/3bigfoot-vs-zombies.jpg",
         Titre: "Bigfoot contre Zombies",
         Resume: "Un scientifique crée un cocktail toxique qui est accidentellement libéré dans la population locale et dans une ferme de corps de la police",
         Acteurs: ["James Carolus", "Todd Carpenter", "Bob Dennis"],
@@ -26,7 +26,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt5307698/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/4rise-of-the-zombies-.jpg",
+        Image: "images/4rise-of-the-zombies-.jpg",
         Titre: "L'Ascension des Zombies",
         Resume: "Lorsque les morts-vivants envahissent San Francisco, un groupe désespéré survit en se retranchant dans la prison d'Alcatraz.",
         Acteurs: ["Ethan Suplee", "Mariel Hemingway", "LeVar Burton"],
@@ -34,7 +34,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt2236182/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/5abraham-lincoln-vs-zombies.jpg",
+        Image: "images/5abraham-lincoln-vs-zombies.jpg",
         Titre: "Abraham Lincoln contre les Zombies",
         Resume: "Le jeune Abe Lincoln de 10 ans voit son père Thomas Lincoln  se suicider à ses côtés. Il va tous les tuer seul... contre tous.",
         Acteurs: ["Bill Oberst Jr", "Kent Igleheart", "Rhianna Van Helton"],
@@ -42,7 +42,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt2246549/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/6navy-seals-vs-zombies.jpg",
+        Image: "images/6navy-seals-vs-zombies.jpg",
         Titre: "Navy Seals contre Zombies",
         Resume: "Les Navy SEALS contre une énorme armée de zombies. Que feront-ils pour survivre, sauver leur ville et libérer les zombies de cette zone ?",
         Acteurs: ["Stephanie Honoré", "Molly Hagan", "Ed Quinn"],
@@ -50,7 +50,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt4511566/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/7cowboy-zombies.jpg",
+        Image: "images/7cowboy-zombies.jpg",
         Titre: "Cowboy Zombies",
         Resume: "La civilisation a été ravagée par une guerre nucléaire et les morts-vivants se sont levés pour attaquer ce qu'il reste de l'humanité. ",
         Acteurs: ["Jarod Anderson", "Greg Bronson", "Kiera Crouch"],
@@ -58,7 +58,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt2888068/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/8humans-vs-zombies.jpg",
+        Image: "images/8humans-vs-zombies.jpg",
         Titre: "Humains contre Zombies",
         Resume: " Les étudiants en été sont exposés à un virus mortel qui se propage rapidement par contact humain direct.",
         Acteurs: ["Christine Bently", "Cody Callahan", "David Blackwell"],
@@ -66,7 +66,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt1815746/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/9zombies-have-fallen.jpg",
+        Image: "images/9zombies-have-fallen.jpg",
         Titre: "Les Zombies Sont Tombés",
         Resume: "Le film raconte l'histoire de Kyra (Tansy Parkinson), qui possède des pouvoirs surnaturels et des visions d'une apocalypse à venir",
         Acteurs: ["Finn H Drude", "Tony Gardner", "Heath Hampson"],
@@ -74,7 +74,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt4974254/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/10attack-of-the-lederhosen-zombies.jpg",
+        Image: "images/10attack-of-the-lederhosen-zombies.jpg",
         Titre: "Les Zombies font du ski",
         Resume: " Action zombie dans les Alpes : un groupe de jeunes snowboarders est coincé dans une station de ski de montagne isolée...",
         Acteurs: ["Gabriela Marcinková", "Laurie Calvert", "Patricia Aulitzky"],
@@ -82,7 +82,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt3569970/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/11lowriders-vs-zombies-from-space.jpg",
+        Image: "images/11lowriders-vs-zombies-from-space.jpg",
         Titre: "Lowriders contre les Zombies de l'Espace",
         Resume: " Une étrange substance extraterrestre est introduite, transformant les gens en zombies extraterrestres, qui va les arrêter?",
         Acteurs: ["Michael Cristian", "Katusha Robert", "Cristina Cruz Rodríguez"],
@@ -90,7 +90,7 @@ let films = [
         Trailer: "https://www.imdb.com/title/tt7118702/?ref_=fn_al_tt_1"
     },
     {
-        Image: "../images/12attack-of-the-southern-fried-zombies.jpg",
+        Image: "images/12attack-of-the-southern-fried-zombies.jpg",
         Titre: "L'Attaque des Zombies du Sud Croustillant",
         Resume: "Un pilote d'avion de traitement, qui doit diriger un groupe hétéroclite de survivants pour échapper à la horde de zombies mortelle",
         Acteurs: ["Miles Doleac", "Michael Emery", "Jeremy Sande"],
@@ -160,7 +160,7 @@ let header = document.querySelector('.start');
     // Image img for img
     let img = document.createElement('img');
     img.className = 'start__header__img';
-    img.setAttribute('src', '../images/icon-zombie.png');
+    img.setAttribute('src', 'images/icon-zombie.png');
     img.setAttribute('alt', 'icon');
 
     // h1 for Title
